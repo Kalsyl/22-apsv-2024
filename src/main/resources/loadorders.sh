1aa844813c89fe60f8e2674a009f6042a247d49f
Kalsyl <lanubedediegoar@gmail.com>
2024-12-03 12:45:12 +0100

    Trans order

--- src/main/resources/loadorders.sh
+++ src/main/resources/loadorders.sh
@@ -1,3 +1,8 @@
 while IFS= read -r line; do
 curl -H "Content-Type: application/json" -X PUT --data-binary "$line" http://localhost:8080/transportationorders;
 done < orders.json
+
+
+Get-Content -Path "orders.json" | ForEach-Object {
+    Invoke-RestMethod -Uri "http://localhost:8080/transportationorders" -Method Put -Body $_ -ContentType "application/json"
+}
